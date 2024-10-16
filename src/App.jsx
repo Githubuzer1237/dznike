230a39594861babdee6b71127e0e9a19389b04f7
Githubuzer1237 <158813082+Githubuzer1237@users.noreply.github.com>
2024-10-16 15:01:37 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Card from './Card'
+import Card2 from './Card2'
+
+function App() {
+  return (
+    <>
+    <div className="cont">
+    <Card/>
+    <Card2/>
+    <Card/>
+    <Card2/>
+    <Card/>
+    <Card2/>
+    <Card/>
+    <Card2/>
+    <Card/>
+    <Card2/>
+    <Card/>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,64 @@
+import './App.css'
+
+const Card = () => {
+  return (
+    <>
+        <div className="container">
+      <div className="cards">
+        <div className="card-1">
+        <img className='cross' src="https://i.ibb.co/cNWqxGx/red.png" alt="" />
+        <h3>Nike X OFF-white</h3>
+        <p>The 10: Air Jordan 1 off-white - Chicago</p>
+        <h4>$999</h4>
+        <div className="btns">
+          <button className="btn-1">
+            Buy now
+          </button>
+          <img className='fav' src="/favourite.png" alt="" />
+        </div>
+        </div>
+
+        <div className="card-2">
+        <img className='cross' src="/nike-blue.png" alt="" />
+        <h3>Nike X OFF-white</h3>
+        <p>The 10: Air Jordan 1 off-white - Chicago</p>
+        <h4>$999</h4>
+        <div className="btns">
+          <button className="btn-1">
+            Buy now
+          </button>
+          <img className='fav' src="/favourite.png" alt="" />
+        </div>
+        </div>
+        <div className="card-1">
+        <img className='cross' src="https://i.ibb.co/cNWqxGx/red.png" alt="" />
+        <h3>Nike X OFF-white</h3>
+        <p>The 10: Air Jordan 1 off-white - Chicago</p>
+        <h4>$999</h4>
+        <div className="btns">
+          <button className="btn-1">
+            Buy now
+          </button>
+          <img className='fav' src="/favourite.png" alt="" />
+        </div>
+        </div>
+
+        <div className="card-2">
+        <img className='cross' src="/nike-blue.png" alt="" />
+        <h3>Nike X OFF-white</h3>
+        <p>The 10: Air Jordan 1 off-white - Chicago</p>
+        <h4>$999</h4>
+        <div className="btns">
+          <button className="btn-1">
+            Buy now
+          </button>
+          <img className='fav' src="/favourite.png" alt="" />
+        </div>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Card
